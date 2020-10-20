7f249f5e7d2c66de8eeb87d7f37df85aa8bc38df
Namlongcomputer <hoandm91@gmail.com>
2020-10-20 18:56:47 +0700

    Updates

---
+++ precache-manifest.10a63295cdb63ef52cacc666adcee409.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d253d845b5dead14813750c15b0c8765",
+    "url": "/react-app-calculator/index.html"
+  },
+  {
+    "revision": "d163fa535730e192d204",
+    "url": "/react-app-calculator/static/css/main.01b45aa2.chunk.css"
+  },
+  {
+    "revision": "a5c17309bcb1f0a96490",
+    "url": "/react-app-calculator/static/js/2.598b524d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-app-calculator/static/js/2.598b524d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d163fa535730e192d204",
+    "url": "/react-app-calculator/static/js/main.711f5386.chunk.js"
+  },
+  {
+    "revision": "51f6787e801f3600d57b",
+    "url": "/react-app-calculator/static/js/runtime-main.12fffe04.js"
+  }
+]);
